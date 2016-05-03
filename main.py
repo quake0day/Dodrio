0f8248c3f8e9ea68d4023ad49151603750e8eb0f
quake0day <quake0day@gmail.com>
2016-05-03 16:37:29 -0400

    add wcg badge

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-
+from wcg import init, getBadges, getRanks
 
 # configuration
 DATABASE = './db/information.db'
@@ -30,7 +30,9 @@ def index():
     cur = g.db.execute('select * from entries order by year desc')
     entries = [dict(id=row[0], type=row[1], title=row[2], author=row[3], confname=row[4], urlpaper=row[5], urlslides=row[6], urlcite=row[7], cite=row[8], place=row[9], year=row[10], text=row[11]) for row in cur.fetchall()]
     #entries = []
-    return render_template('index.html', entries=entries)
+    soup = init()
+    res = getBadges(soup)
+    return render_template('index.html', entries=entries, badges = res)
 
 @app.errorhandler(404)
 def page_not_found(error):
---
+++ wcg.py
@@ -0,0 +1,38 @@
+import urllib2
+import BeautifulSoup as bs
+import re
+
+
+def init():
+    URL = "http://signature.statseb.fr/index.py?cpid=c60f07487dde49e4caa5a251579ef544"
+    r = urllib2.urlopen(URL)
+    soup = bs.BeautifulSoup(r)
+    return soup
+
+
+def getBadges(soup):
+    IMG_URL_BASE = "http://signature.statseb.fr/"
+    imgs = soup.findAll("img")
+    res = []
+    for img in imgs:
+        # print img
+        title = img.get('title', '')
+        src = img.get('src', '')
+        full_url = IMG_URL_BASE + src
+        if title:
+            res.append([title, full_url])
+    return res
+
+
+def getRanks(soup):
+    # print soup
+    world = soup.find(text=re.compile("World"))
+    worldPos = world.next.string
+
+    county = soup.find(text=re.compile("Country position"))
+    countyPos = county.next.string
+    return worldPos, countyPos
+
+
+# print getBadges(soup)
+# print getRanks(soup)
